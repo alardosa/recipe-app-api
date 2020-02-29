d4b4b6857fe8fce9edffd9bca5385c8b0fcad839
root <alardosa@gmail.com>
2020-02-29 19:56:59 +0800

    test import random

--- app/app/test.py
+++ app/app/test.py
@@ -1,6 +1,5 @@
-from math import pi
+import random
 
-# Note that in the above example,  
-# we used math.pi. Here we have used 
-# pi directly. 
-print(pi) 
+
+for i in range(10):
+    print(random.randint(1, 25))
