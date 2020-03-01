33516446615ad4293bbe81b5967940577cf14028
root <alardosa@gmail.com>
2020-03-01 16:28:08 +0800

    test 19

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,9 @@
-def add(x, y):
+def add_function(x, y):
     """Add two numbers together and return the result"""
     return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return x - y
+    pass
--- test.py
+++
@@ -1,1 +0,0 @@
-print('Hi Al Ardosaa')
