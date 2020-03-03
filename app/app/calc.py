1c50c84d879bd783ceea901eb0f7bbc576f2249d
root <alardosa@gmail.com>
2020-03-03 09:50:54 +0800

    Configure custom user model - Create Core App

--- app/app/calc.py
+++
@@ -1,9 +0,0 @@
-def add(x, y):
-    """Add two numbers together and return the result"""
-    return x + y
-
-
-def subtract(x, y):
-    """Subtract x from y and return result"""
-    return x - y
-    pass
